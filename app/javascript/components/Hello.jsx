eb46e3123ce7b73f651ef4ef70cdbf91ffbb5774
Theodor Vararu <theo@vararu.org>
2017-03-08 22:48:55 +0000

    Update example, use counter, update .babelrc

--- app/javascript/components/Hello.jsx
+++ app/javascript/components/Hello.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+export default class Hello extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { seconds: 0 }
+  }
 
-Hello.defaultProps = {
-  name: 'World'
+  componentDidMount () {
+    this.$interval = setInterval(() => {
+      this.setState(({ seconds }) => { return { seconds: seconds + 1 } })
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.$interval)
+  }
+
+  render () {
+    const s = this.state.seconds
+    return <div>{ s } second{ s === 1 ? '' : 's' } ha{ s === 1 ? 's' : 've' } passed.</div>
+  }
 }
-
-Hello.propTypes = {
-  name: React.PropTypes.string
-}
-
-export default Hello
